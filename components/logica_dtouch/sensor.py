86cb45ee564e8ca6fd4657f1383b96dc0dc1bf7b
sasodoma <saso.doma@gmail.com>
2024-05-04 20:21:44 +0200

    Attempt to declare sensors in for loop

--- components/logica_dtouch/sensor.py
+++ components/logica_dtouch/sensor.py
@@ -24,24 +24,25 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(LOGICA_dTouch),
-            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-                unit_of_measurement=UNIT_CELSIUS,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_TEMPERATURE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_MOISTURE_CONTENT): sensor.sensor_schema(
-                unit_of_measurement=UNIT_PERCENT,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_MOISTURE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_EQUIVALENT_MOISTURE_CONTENT): sensor.sensor_schema(
-                unit_of_measurement=UNIT_PERCENT,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_MOISTURE,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
+            # cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+            #     unit_of_measurement=UNIT_CELSIUS,
+            #     accuracy_decimals=1,
+            #     device_class=DEVICE_CLASS_TEMPERATURE,
+            #     state_class=STATE_CLASS_MEASUREMENT,
+            # ),
+            # cv.Optional(CONF_MOISTURE_CONTENT): sensor.sensor_schema(
+            #     unit_of_measurement=UNIT_PERCENT,
+            #     accuracy_decimals=1,
+            #     device_class=DEVICE_CLASS_MOISTURE,
+            #     state_class=STATE_CLASS_MEASUREMENT,
+            # ),
+            # cv.Optional(CONF_EQUIVALENT_MOISTURE_CONTENT): sensor.sensor_schema(
+            #     unit_of_measurement=UNIT_PERCENT,
+            #     accuracy_decimals=1,
+            #     device_class=DEVICE_CLASS_MOISTURE,
+            #     state_class=STATE_CLASS_MEASUREMENT,
+            # ),
+            cv.Optional("num_probes", default=0): cv.int,
             cv.Optional(CONF_ADDRESS, default=1): cv.int_range(min=1, max=254),
         }
     )
@@ -55,16 +56,10 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    if CONF_TEMPERATURE in config:
-        sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
+    for idx in range(0, config["num_probes"]):
+        sens = await sensor.new_sensor({
+            "name": "temp" + str(idx)
+        })
         cg.add(var.set_temperature_sensor(sens))
-
-    if CONF_MOISTURE_CONTENT in config:
-        sens = await sensor.new_sensor(config[CONF_MOISTURE_CONTENT])
-        cg.add(var.set_mc_sensor(sens))
-
-    if CONF_EQUIVALENT_MOISTURE_CONTENT in config:
-        sens = await sensor.new_sensor(config[CONF_EQUIVALENT_MOISTURE_CONTENT])
-        cg.add(var.set_emc_sensor(sens))
 
     cg.add(var.set_address(config[CONF_ADDRESS]))
