b70f49449f01cd99d2f198cd99608dc67ca4cdb9
sasodoma <saso.doma@gmail.com>
2024-05-04 22:34:24 +0200

    Code generation works

--- components/logica_dtouch/sensor.py
+++ components/logica_dtouch/sensor.py
@@ -15,6 +15,7 @@ from esphome.const import (
 
 CONF_MOISTURE_CONTENT = "moisture_content"
 CONF_EQUIVALENT_MOISTURE_CONTENT = "equivalent_moisture_content"
+CONF_NUM_PROBES = "num_probes"
 
 DEPENDENCIES = ["uart"]
 
@@ -25,25 +26,36 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(LOGICA_dTouch),
-            # cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
-            #     unit_of_measurement=UNIT_CELSIUS,
-            #     accuracy_decimals=1,
-            #     device_class=DEVICE_CLASS_TEMPERATURE,
-            #     state_class=STATE_CLASS_MEASUREMENT,
-            # ),
+            cv.Optional(CONF_TEMPERATURE): sensor.sensor_schema(
+                unit_of_measurement=UNIT_CELSIUS,
+                accuracy_decimals=1,
+                device_class=DEVICE_CLASS_TEMPERATURE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ).extend(cv.Schema(
+                {
+                    cv.Required(CONF_NUM_PROBES): cv.int_range(min=1)
+                }
+            )),
             cv.Optional(CONF_MOISTURE_CONTENT): sensor.sensor_schema(
                 unit_of_measurement=UNIT_PERCENT,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_MOISTURE,
                 state_class=STATE_CLASS_MEASUREMENT,
-            ),
+            ).extend(cv.Schema(
+                {
+                    cv.Required(CONF_NUM_PROBES): cv.int_range(min=1)
+                }
+            )),
             cv.Optional(CONF_EQUIVALENT_MOISTURE_CONTENT): sensor.sensor_schema(
                 unit_of_measurement=UNIT_PERCENT,
                 accuracy_decimals=1,
                 device_class=DEVICE_CLASS_MOISTURE,
                 state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional("num_probes", default=0): cv.int_,
+            ).extend(cv.Schema(
+                {
+                    cv.Required(CONF_NUM_PROBES): cv.int_range(min=1)
+                }
+            )),
             cv.Optional(CONF_ADDRESS, default=1): cv.int_range(min=1, max=254),
         }
     )
@@ -62,28 +74,49 @@ async def to_code(config):
     if CONF_MOISTURE_CONTENT in config:
         sens = await sensor.new_sensor(config[CONF_MOISTURE_CONTENT])
         cg.add(var.set_mc_sensor(sens))
+        for idx in range(0, config[CONF_MOISTURE_CONTENT][CONF_NUM_PROBES]):
+            custom_conf = config[CONF_MOISTURE_CONTENT].copy()
+            custom_conf[CONF_ID] = ID(
+                    id=config[CONF_MOISTURE_CONTENT][CONF_ID].id + "_" + str(idx),
+                    is_declaration=config[CONF_MOISTURE_CONTENT][CONF_ID].is_declaration,
+                    is_manual=config[CONF_MOISTURE_CONTENT][CONF_ID].is_manual,
+                    type=config[CONF_MOISTURE_CONTENT][CONF_ID].type
+            )
+            custom_conf["name"] = config[CONF_MOISTURE_CONTENT]["name"] + "_" + str(idx)
+            print(custom_conf)
+            sens = await sensor.new_sensor(custom_conf)
+            cg.add(var.set_mc_sensor(sens))
 
     if CONF_EQUIVALENT_MOISTURE_CONTENT in config:
-        print(type(config[CONF_EQUIVALENT_MOISTURE_CONTENT]))
-        print(type(config[CONF_EQUIVALENT_MOISTURE_CONTENT]['state_class']))
-        print(config[CONF_EQUIVALENT_MOISTURE_CONTENT])
         sens = await sensor.new_sensor(config[CONF_EQUIVALENT_MOISTURE_CONTENT])
         cg.add(var.set_emc_sensor(sens))
+        for idx in range(0, config[CONF_EQUIVALENT_MOISTURE_CONTENT][CONF_NUM_PROBES]):
+            custom_conf = config[CONF_EQUIVALENT_MOISTURE_CONTENT].copy()
+            custom_conf[CONF_ID] = ID(
+                    id=config[CONF_EQUIVALENT_MOISTURE_CONTENT][CONF_ID].id + "_" + str(idx),
+                    is_declaration=config[CONF_EQUIVALENT_MOISTURE_CONTENT][CONF_ID].is_declaration,
+                    is_manual=config[CONF_EQUIVALENT_MOISTURE_CONTENT][CONF_ID].is_manual,
+                    type=config[CONF_EQUIVALENT_MOISTURE_CONTENT][CONF_ID].type
+            )
+            custom_conf["name"] = config[CONF_EQUIVALENT_MOISTURE_CONTENT]["name"] + "_" + str(idx)
+            print(custom_conf)
+            sens = await sensor.new_sensor(custom_conf)
+            cg.add(var.set_emc_sensor(sens))
 
-    for idx in range(0, config["num_probes"]):
-        custom_conf = {
-            CONF_ID: ID(id="temp" + str(idx), is_declaration=True, is_manual=True, type=type_sensor),
-            'name': "temp" + str(idx),
-            'disabled_by_default': False,
-            'force_update': False,
-            'unit_of_measurement': '°C',
-            'accuracy_decimals': 1,
-            'device_class': 'temperature',
-            'state_class': config[CONF_EQUIVALENT_MOISTURE_CONTENT]['state_class']
-        }
-        print(custom_conf)
-        print(type(custom_conf['state_class']))
-        sens = await sensor.new_sensor(custom_conf)
-        cg.add(var.set_temperature_sensor(sens))
+    if CONF_TEMPERATURE in config:
+        sens = await sensor.new_sensor(config[CONF_TEMPERATURE])
+        cg.add(var.set_emc_sensor(sens))
+        for idx in range(0, config[CONF_TEMPERATURE][CONF_NUM_PROBES]):
+            custom_conf = config[CONF_TEMPERATURE].copy()
+            custom_conf[CONF_ID] = ID(
+                    id=config[CONF_TEMPERATURE][CONF_ID].id + "_" + str(idx),
+                    is_declaration=config[CONF_TEMPERATURE][CONF_ID].is_declaration,
+                    is_manual=config[CONF_TEMPERATURE][CONF_ID].is_manual,
+                    type=config[CONF_TEMPERATURE][CONF_ID].type
+            )
+            custom_conf["name"] = config[CONF_TEMPERATURE]["name"] + "_" + str(idx)
+            print(custom_conf)
+            sens = await sensor.new_sensor(custom_conf)
+            cg.add(var.set_temperature_sensor(sens))
 
     cg.add(var.set_address(config[CONF_ADDRESS]))
