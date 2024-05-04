bbf80f98803a7aba4cfea8c9b71e9d5073bc648b
sasodoma <saso.doma@gmail.com>
2024-05-04 21:24:42 +0200

    Print state class

--- components/logica_dtouch/sensor.py
+++ components/logica_dtouch/sensor.py
@@ -64,7 +64,7 @@ async def to_code(config):
         cg.add(var.set_mc_sensor(sens))
 
     if CONF_EQUIVALENT_MOISTURE_CONTENT in config:
-        print(config[CONF_EQUIVALENT_MOISTURE_CONTENT])
+        print(config[CONF_EQUIVALENT_MOISTURE_CONTENT]['state_class'])
         sens = await sensor.new_sensor(config[CONF_EQUIVALENT_MOISTURE_CONTENT])
         cg.add(var.set_emc_sensor(sens))
 
