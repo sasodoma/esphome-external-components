1adc97daf2831437fac2330e379e53ea7e64d026
sasodoma <saso.doma@gmail.com>
2025-02-02 16:07:01 +0100

    Multiply all BL0937 factors by two, this is a hack, because my T34 chip reads every pulse twice.

--- components/hlw8012/hlw8012.cpp
+++ components/hlw8012/hlw8012.cpp
@@ -21,9 +21,9 @@ void HLW8012Component::setup() {
   if (this->sensor_model_ == HLW8012_SENSOR_MODEL_BL0937) {
     reference_voltage = 1.218f;
     this->power_multiplier_ =
-        reference_voltage * reference_voltage * this->voltage_divider_ / this->current_resistor_ / 1721506.0f;
-    this->current_multiplier_ = reference_voltage / this->current_resistor_ / 94638.0f;
-    this->voltage_multiplier_ = reference_voltage * this->voltage_divider_ / 15397.0f;
+        reference_voltage * reference_voltage * this->voltage_divider_ / this->current_resistor_ / 3443012.0f;
+    this->current_multiplier_ = reference_voltage / this->current_resistor_ / 189276.0f;
+    this->voltage_multiplier_ = reference_voltage * this->voltage_divider_ / 30794.0f;
   } else {
     // HLW8012 and CSE7759 have same reference specs
     reference_voltage = 2.43f;
